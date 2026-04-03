2dbbae129ccef9f5cfebcd34979a73a8146fd955
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-04-04 00:12:18 +0530

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+
+        // init anagramMap
+        let anagramMap = {};
+
+        // run loop from i=0 to i<strs.length
+        for(let i=0;i<strs.length;i++){
+            // init charArray
+            let charArray = new Array(26).fill(0);
+            // run loop from j=0 to j<strs[i].length
+            for(let j=0;j<strs[i].length;j++){
+                // increment charArray for character at strs[i][j]
+                charArray[strs[i].charCodeAt(j)-97]++;
+            }
+            let charString = charArray.join(",");
+            // if anagramMap contains joined string of charArray
+            if(anagramMap[charString]){
+                // push strs[i] to anagramMap[character string]
+                anagramMap[charString].push(strs[i]);
+            }
+            // else 
+            else{
+                // add [strs[i]] to anagramMap[character string]
+                anagramMap[charString] = [strs[i]];
+            }
+        }
+
+        // return Object.values(anagramMap)
+        return Object.values(anagramMap)
+        
+    }
+}
