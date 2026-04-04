aabf032279bd272ee60dca13bd0513d669bf1a8f
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-04-05 03:11:25 +0530

    Add: sort-colors - submission-4

---
+++ Data Structures & Algorithms/sort-colors/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        let start = 0, end = nums.length-1;
+        for(let i=0;i<=end;i++){
+            if(nums[i] == 0){
+                [nums[start],nums[i]] = [nums[i],nums[start]];
+                start++;
+            }
+            else if(nums[i] == 2){
+                [nums[end],nums[i]] = [nums[i],nums[end]];
+                end--;
+                i--;
+            }
+        }
+    }
+}
