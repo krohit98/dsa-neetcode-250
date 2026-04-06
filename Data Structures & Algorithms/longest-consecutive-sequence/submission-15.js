bd15e03d4527b506494167290d255f687cdcd3ff
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-04-07 03:04:44 +0530

    Add: longest-consecutive-sequence - submission-15

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-15.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        let maxCount = 0;
+        let numsMap = {};
+        for(let i=0;i<nums.length;i++){
+            numsMap[nums[i]] = 'unvisited';
+        }
+        for(let i=0;i<nums.length;i++){
+            if(numsMap[nums[i]] == 'visited') continue;
+            let count = 1;
+            let num = nums[i];
+            numsMap[num] = 'visited';
+            while(numsMap[++num]) {
+                numsMap[num] = 'visited'
+                count++;
+            }
+            num = nums[i];
+            while(numsMap[--num]) {
+                numsMap[num] = 'visited'
+                count++;
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+        return maxCount;
+    }
+}
