4fafe4cc2e18fddd6b3f3b4d29174f8f45913e83
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-04-08 03:25:20 +0530

    Update: longest-consecutive-sequence - submission-16

--- Data Structures & Algorithms/longest-consecutive-sequence/submission-16.js
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-16.js
@@ -4,26 +4,16 @@ class Solution {
      * @return {number}
      */
     longestConsecutive(nums) {
+        let numsSet = new Set(nums);
         let maxCount = 0;
-        let numsMap = {};
         for(let i=0;i<nums.length;i++){
-            numsMap[nums[i]] = 'unvisited';
-        }
-        for(let i=0;i<nums.length;i++){
-            if(numsMap[nums[i]] == 'visited') continue;
-            let count = 1;
+            if(numsSet.has(nums[i]-1)) continue;
             let num = nums[i];
-            numsMap[num] = 'visited';
-            while(numsMap[++num]) {
-                numsMap[num] = 'visited'
+            let count = 1;
+            while(numsSet.has(++num)){
                 count++;
             }
-            num = nums[i];
-            while(numsMap[--num]) {
-                numsMap[num] = 'visited'
-                count++;
-            }
-            maxCount = Math.max(maxCount, count);
+            maxCount = Math.max(maxCount,count);
         }
         return maxCount;
     }
