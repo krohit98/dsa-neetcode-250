1a6cade00d11c509268fd2b4cb40c03f0036c69d
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-04-09 02:28:20 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let sum = 0, i=0;
+        while(i<prices.length-1){
+            while(i<prices.length-1 && prices[i+1] <= prices[i]){
+                i++;
+            }
+            let buy = prices[i];
+            while(i<prices.length-1 && prices[i+1] >= prices[i]){
+                i++;
+            }
+            sum += (prices[i] - buy);
+        }
+        return sum;
+    }
+}
