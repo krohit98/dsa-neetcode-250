5b82d732257aeee36d83796fa5a21fc3f4ba6791
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-04-12 22:23:42 +0530

    Add: baseball-game - submission-4

---
+++ Data Structures & Algorithms/baseball-game/submission-4.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} operations
+     * @return {number}
+     */
+    calPoints(operations) {
+        // BRUTE FORCE
+        let record = [];
+        for(let i=0;i<operations.length;i++){
+            if(operations[i] == '+'){
+                record.push(Number(record[record.length-1])+Number(record[record.length-2]));
+            }
+            else if(operations[i] == 'C'){
+                record.pop();
+            }
+            else if(operations[i] == 'D'){
+                record.push(record[record.length-1] * 2)
+            }
+            else{
+                record.push(operations[i]);
+            }
+        }
+        console.log(record)
+        return record.reduce((total,i) => {
+            return total+Number(i);
+        },0)
+    }
+}
