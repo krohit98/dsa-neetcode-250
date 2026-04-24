e5b86d70ef96dd26dde2e6f23b1635f2733227de
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-04-24 17:09:30 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {number}
+     */
+    mySqrt(x) {
+        let start = 1, end = x;
+        while(start<=end){
+            let mid = Math.floor((start+end)/2);
+            let square = mid*mid;
+            if(square < x) start = mid+1;
+            else if(square > x) end = mid-1;
+            else return mid;
+        }
+        return end;
+    }
+}
