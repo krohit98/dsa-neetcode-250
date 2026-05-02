80934a2bea76f4cb54ef45e21724bf04a8a279fb
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-05-02 22:37:19 +0530

    Add: capacity-to-ship-packages-within-d-days - submission-10

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-10.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number[]} weights
+     * @param {number} days
+     * @return {number}
+     */
+    shipWithinDays(weights, days) {
+        let maxWeight = 0; 
+        let weightSum = 0;
+        for(let i=0;i<weights.length;i++){
+            weightSum+=weights[i];
+            maxWeight = Math.max(maxWeight, weights[i]);
+        }
+
+        let start = maxWeight, end = weightSum;
+        let capacity = 1;
+
+        while(start <= end){
+            let mid = Math.floor((start+end)/2);
+            let dayCount = 0, shipped = 0;
+            for(let i=0;i<weights.length;i++){
+                if(shipped+weights[i]<=mid){
+                    shipped+=weights[i];
+                }
+                else{
+                    dayCount++;
+                    shipped = weights[i];
+                }
+            }
+            dayCount++;
+
+            if(dayCount <= days){
+                capacity = mid;
+                end = mid-1;
+            }
+            else start = mid+1;
+        }
+
+        return capacity;
+    }
+}
