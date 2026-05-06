b36359b4f8b93cfa4a077f9f9e58198495492806
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-05-06 20:54:33 +0530

    Add: search-in-rotated-sorted-array-ii - submission-8

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-8.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums, target) {
+        let start = 0, end = nums.length-1;
+        while(start<=end){
+            console.log('start:',nums[start], ' end:', nums[end])
+            let mid = Math.floor((start+end)/2);
+            if(nums[mid] == target) return true;
+
+            while(nums[start] == nums[mid] && nums[mid] == nums[end]){
+                start++;
+                end--;
+            }
+
+            if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target < nums[mid]) end = mid-1;
+                else start = mid+1;
+            }
+            else{
+                if(nums[mid] < target && target <= nums[end]) start = mid+1;
+                else end = mid-1;
+            }
+        }
+        return false;
+    }
+}
