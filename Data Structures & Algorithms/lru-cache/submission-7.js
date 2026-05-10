748867d64a4b54b06361c92c206038fbf333b96e
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-05-10 15:21:40 +0530

    Add: lru-cache - submission-7

---
+++ Data Structures & Algorithms/lru-cache/submission-7.js
@@ -0,0 +1,80 @@
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.map = new Map();
+        this.dummy = new ListNode();
+        this.front = this.dummy;
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        if(this.map.has(key)) {
+            let node = this.map.get(key);
+            if(this.front != node){
+                this.disconnectNode(node);
+                this.moveNodeToFront(node);
+            }
+            console.log(`get ${key}: `, this.dummy)
+            return node.val;
+        }
+        else return -1;
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        let node;
+        if(this.map.has(key)){
+            node = this.map.get(key);
+            node.val = value;
+            if(this.front != node) this.disconnectNode(node);
+        }
+        else{
+            node = new ListNode(key, value);
+            if(this.map.size == this.capacity){
+                this.removeLRUNode();
+            }
+        }
+        this.map.set(key, node);
+        if(this.front != node) this.moveNodeToFront(node);
+        console.log(`put ${key}: `, this.dummy)
+    }
+
+    disconnectNode(node){
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+        node.next = null; 
+    }
+
+    moveNodeToFront(node){
+        this.front.next = node;
+        node.prev = this.front;
+        this.front = node;
+    }
+
+    removeLRUNode(){
+        this.map.delete(this.dummy.next.key)
+        this.dummy.next = this.dummy.next.next;
+        if(this.dummy.next) this.dummy.next.prev = this.dummy;
+        else this.front = this.dummy;
+        console.log(this.dummy)
+    }
+}
+
+class ListNode {
+    constructor(key = null, val = null, next = null, prev = null){
+        this.key = key;
+        this.val = val;
+        this.next = next;
+        this.prev = prev;
+    }
+}
