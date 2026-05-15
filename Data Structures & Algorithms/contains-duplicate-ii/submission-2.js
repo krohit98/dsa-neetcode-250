20ba7cfef34db75d8056821ea20532551f38899f
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-05-15 21:04:57 +0530

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        let duplicateMap = {};
+
+        for(let i=0;i<nums.length;i++){
+            console.log(duplicateMap[nums[i]])
+            if(duplicateMap[nums[i]] && Math.abs(parseInt(duplicateMap[nums[i]]) - i) <= k) return true;
+            else duplicateMap[nums[i]] = `${i}`;
+        }
+        return false;
+    }
+}
