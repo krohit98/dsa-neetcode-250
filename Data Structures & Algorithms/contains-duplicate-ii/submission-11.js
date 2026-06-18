4e4ffc110c0b55c8a61ef041f1105f58ef24944e
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-06-18 13:13:56 +0530

    Add: contains-duplicate-ii - submission-11

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-11.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        // init empty set
+        let range = new Set();
+        // init start to 0
+        let start = 0;
+
+        // run loop for end = 0 to end < nums.length
+        for(let end=0; end<nums.length; end++){
+            // check if length of set equals k+1
+            if(end - start > k){
+                // remove nums[start] from set
+                range.delete(nums[start])
+                // increment start
+                start++;
+            }
+            // check if nums[end] exists in set
+            if(range.has(nums[end]))
+                // return true
+                return true;
+            // else
+            else
+                // add nums[end] to set
+                range.add(nums[end]);
+        }
+
+        // return false
+        return false;
+    }
+}
