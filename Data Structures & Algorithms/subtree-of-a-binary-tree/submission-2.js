34c6e86d7d062a5885216381f8385ba3fd59a957
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-06-23 02:36:32 +0530

    Add: subtree-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-2.js
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+    isSubtree(root, subRoot) {
+        // init empty stack
+        let stack = [];
+        // init curr to root
+        let curr = root;
+
+        // run loop while stack is not empty and curr is not null
+        while(stack.length || curr){
+            // run loop while curr is not empty
+            while(curr){
+                // check if curr.val is same as subroot.val
+                if(curr.val === subRoot.val){
+                    // init isSame to return value from isSameTree(curr, subroot)
+                    let isSame = this.isSameTree(curr, subRoot)
+                    // if isSame is true, return true
+                    if(isSame) return true;
+                }
+                // push curr to stack
+                stack.push(curr);
+                // update curr to curr.left
+                curr = curr.left;
+            }
+
+            // pop stack and update curr
+            curr = stack.pop();
+
+            // update curr to curr.right
+            curr = curr.right;
+        }
+
+        // return false
+        return false;
+    }
+
+    isSameTree(p, q){
+        if(!p && !q) return true;
+        else if((p && !q) || (!p && q)) return false;
+        else if(p.val !== q.val) return false;
+        else return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
+    }
+}
