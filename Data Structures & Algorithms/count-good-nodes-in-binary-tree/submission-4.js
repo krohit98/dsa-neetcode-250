79d26da843c536a470c7fd0005d76c2d0df4a5a0
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-07-09 15:41:30 +0530

    Add: count-good-nodes-in-binary-tree - submission-4

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-4.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    goodNodes(root) {
+        // init result to 0
+        let goodNodeCount = 0;
+        // init stack to empty array
+        let stack = [];
+        // init curr to root
+        let curr = root;
+        // init largest to 0
+        let largest = null;
+
+        // run loop while stack is not empty or curr is not null
+        while(stack.length || curr){
+            // run loop while curr is not null
+            while(curr){
+                // check if stack is empty and largest is 0
+                if(largest == null || largest <= curr.val){
+                        // set largest to curr.val
+                        largest = curr.val;
+                        // increment result count
+                        goodNodeCount++;
+                }
+
+                // push [curr, largest] in stack
+                stack.push([curr, largest]);
+                // set curr = curr.left
+                curr = curr.left;
+            }
+
+            // pop stack and update [curr, largest]
+           [curr, largest] = stack.pop();
+            // set curr to curr.right
+            curr = curr.right;
+        }
+
+        // return result
+        return goodNodeCount;
+    }
+}
