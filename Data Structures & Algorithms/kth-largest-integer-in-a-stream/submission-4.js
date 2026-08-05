3418361bd82fbde125a73670b892f290597088d8
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-08-06 00:56:51 +0530

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.js
@@ -0,0 +1,24 @@
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.pq = new MinPriorityQueue();
+        this.k = k;
+        for(let i=0;i<nums.length;i++) {
+            this.pq.push(nums[i]);
+        }
+        while(this.pq.size() > this.k) this.pq.dequeue();
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        this.pq.push(val);
+        if(this.pq.size() > this.k) this.pq.dequeue();
+        return this.pq.front();
+    }
+}
