4812b9858f5b91124f7991bf3276ad1d8f680ec8
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-08-11 12:17:13 +0530

    Add: single-threaded-cpu - submission-8

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-8.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[][]} tasks
+     * @return {number[]}
+     */
+    getOrder(tasks) {
+        let time = 0;
+        let enqueueHeap = new MinPriorityQueue(task => task.enqueueTime)
+        let processHeap = new PriorityQueue((taskA, taskB) => {
+            return taskA.processTime == taskB.processTime ? taskA.index - taskB.index : taskA.processTime - taskB.processTime;
+        })
+
+        for(let i=0; i<tasks.length; i++) {
+            enqueueHeap.enqueue({
+                enqueueTime:tasks[i][0],
+                processTime:tasks[i][1],
+                index:i
+            })
+        }
+
+        time = 0;
+
+        let taskOrder = [];
+
+        while(enqueueHeap.size() || processHeap.size()) {
+
+            if(enqueueHeap.size() && time < enqueueHeap.front().enqueueTime) {
+                time = enqueueHeap.front().enqueueTime
+            }
+
+            while(enqueueHeap.size() && enqueueHeap.front().enqueueTime <= time) {
+                processHeap.enqueue(enqueueHeap.dequeue());
+            }
+
+            if(processHeap.size()) {
+                let selectedTask = processHeap.dequeue();
+                time += selectedTask.processTime;
+                taskOrder.push(selectedTask.index);
+            }
+        }
+
+        return taskOrder;
+    }
+}
