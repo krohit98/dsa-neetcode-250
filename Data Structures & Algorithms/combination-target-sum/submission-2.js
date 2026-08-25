c72e8dd6807acb718567f38a39dc182321bc6f9b
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-08-26 03:36:02 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        let result = [];
+        let subset = [];
+        let sum = 0;
+
+        function backtrack(start) {
+            if(sum == target) {
+                result.push([...subset])
+            }
+
+            for(let i=start; i<nums.length; i++){
+                if(sum + nums[i] <= target) {
+                    subset.push(nums[i]);
+                    sum += nums[i];
+                    backtrack(i);
+                    sum -= nums[i];
+                    subset.pop();
+                }
+            }
+        }
+
+        backtrack(0);
+        return result;
+    }
+}
