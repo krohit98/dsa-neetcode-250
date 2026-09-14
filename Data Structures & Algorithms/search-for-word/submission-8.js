341f7998a728ddee78a65d5560de935b0618dc84
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-09-14 19:43:09 +0530

    Add: search-for-word - submission-8

---
+++ Data Structures & Algorithms/search-for-word/submission-8.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @param {string} word
+     * @return {boolean}
+     */
+    exist(board, word) {
+        function backtrack(index, r, c) {
+            if(index == word.length) {
+                return true;
+            }
+
+            if(r < 0 || c < 0 || r >= board.length || c >= board[0].length) {
+                return false;
+            }
+
+            console.log(board[r][c])
+
+            if(word[index] == board[r][c]) {
+                board[r][c] = "#"
+                let found = backtrack(index+1, r, c+1) ||
+                backtrack(index+1, r+1, c) ||
+                backtrack(index+1, r, c-1) ||
+                backtrack(index+1, r-1, c)
+
+                if(found) return true;
+                board[r][c] = word[index];
+            }
+
+            return false;
+        }
+
+        for(let i=0;i<board.length;i++) {
+            for(let j=0;j<board[i].length;j++) {
+                if(backtrack(0,i,j)) return true;
+            }
+        }
+        return false;
+    }
+}
