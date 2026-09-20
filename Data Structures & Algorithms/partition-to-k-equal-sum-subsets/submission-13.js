e2035b4681e48d2b4abc41e391dbb710a84dcf73
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-09-20 20:28:39 +0530

    Add: partition-to-k-equal-sum-subsets - submission-13

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-13.js
@@ -0,0 +1,45 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    canPartitionKSubsets(nums, k) {
+        let sum = 0;
+        for(let i=0; i<nums.length; i++) {
+            sum += nums[i];
+        }
+
+        if(sum % k !== 0) return false;
+
+        let target = sum / k;
+
+        nums.sort((a,b) => b-a);
+        if(nums[0] > target) return false;
+
+        let used = new Set();
+
+        function backtrack(currentSum, completedBuckets) {
+            if(completedBuckets == k) return true;
+
+            if(currentSum == target) {
+                return backtrack(0, completedBuckets+1)
+            }
+
+            let visited = new Set();
+
+            for(let i=0; i<nums.length;i++) {
+                if(used.has(i) || visited.has(nums[i]) || nums[i]+currentSum > target) continue;
+
+                used.add(i);
+                visited.add(nums[i]);
+                if(backtrack(currentSum+nums[i], completedBuckets)) return true;
+                used.delete(i);
+            }
+
+            return false;
+        }
+        
+        return backtrack(0,0);
+    }
+}
