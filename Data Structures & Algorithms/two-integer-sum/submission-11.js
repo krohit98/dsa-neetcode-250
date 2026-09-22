4e9865e2a9130fda5ea28855a11e100d6e8773e9
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-09-22 22:52:45 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let numMap = new Map();
+
+        for(let i=0; i<nums.length; i++) {
+            if(numMap.has(target - nums[i])) {
+                return [i, numMap.get(target - nums[i])];
+            }
+            else numMap.set(nums[i],i);
+        }
+
+        return [];
+    }
+}
