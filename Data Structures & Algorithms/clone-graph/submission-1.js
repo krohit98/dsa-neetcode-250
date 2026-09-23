2b9936f86df7e8c570cccd69a5030bb3a203466f
Kumar Rohit <47668536+krohit98@users.noreply.github.com>
2026-09-23 19:38:11 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.js
@@ -0,0 +1,36 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val = 0, neighbors = []) {
+ *       this.val = val;
+ *       this.neighbors = neighbors;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        if(!node) return null;
+        let nodeArr = new Array(100);
+
+        function cloneNeighbors(node){
+            let clone = new Node(node.val);
+            nodeArr[node.val] = clone;
+            for(let i=0; i<node.neighbors.length; i++) {
+                if(!nodeArr[node.neighbors[i].val]){
+                    clone.neighbors.push(cloneNeighbors(node.neighbors[i]))
+                }
+                else clone.neighbors.push(nodeArr[node.neighbors[i].val])
+            }
+            return clone;
+        }
+
+        cloneNeighbors(node);
+        return nodeArr[1];
+
+    }
+}
